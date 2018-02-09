aa01f37bebbc349ceccb4bce9603f6535e1c1c44
Charles Crete <cretezy@gmail.com>
2018-02-09 02:34:29 -0500

    Change site underline links, add gitignore

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -6,51 +6,51 @@
  */
 
 /* List of projects/orgs using your project for the users page */
-const users = [
-	{
-		caption: 'User1',
-		image: '/test-site/img/docusaurus.svg',
-		infoLink: 'https://www.facebook.com',
-		pinned: true,
-	},
-];
+// const users = [
+// 	{
+// 		caption: 'User1',
+// 		image: '/test-site/img/docusaurus.svg',
+// 		infoLink: 'https://www.facebook.com',
+// 		pinned: true,
+// 	},
+// ];
 
 const siteConfig = {
-	title: 'Noderize' /* title for your website */,
+	title: 'Noderize',
 	tagline: ' Create a Node app in less than 30 seconds.',
-	url: 'https://noderize.js.org' /* your website url */,
-	baseUrl: '/' /* base url for your project */,
+	url: 'https://cretezy.github.io/noderize',
+	// url: 'https://noderize.js.org',
+	baseUrl: '/noderize/',
+	// baseUrl: '/',
 	projectName: 'noderize',
+
 	headerLinks: [
 		{ doc: 'introduction', label: 'Docs' },
 		{ blog: true, label: 'Blog' },
 		{ href: 'https://github.com/Cretezy/noderize', label: 'GitHub' },
 	],
-	users,
-	/* path to images for header/footer */
+	// users,
 	headerIcon: 'img/icon.svg',
 	footerIcon: 'img/icon.svg',
 	favicon: 'img/favicon/favicon.icon',
-	/* colors for website */
 	colors: {
 		primaryColor: '#343a40',
 		secondaryColor: '#809fc0',
 	},
-	// This copyright info is used in /core/Footer.js and blog rss/atom feeds.
+
 	copyright:
-	'Copyright © ' +
-	new Date().getFullYear() +
-	' Charles Crete',
-	organizationName: 'Cretezy', // or set an env variable ORGANIZATION_NAME
-	projectName: 'noderize', // or set an env variable PROJECT_NAME
+	`Copyright © ${new Date().getFullYear()} Charles Crete`,
+	organizationName: 'Cretezy',
+	projectName: 'noderize',
+
 	highlight: {
 		// Highlight.js theme to use for syntax highlighting in code blocks
 		theme: 'default',
 	},
+
 	scripts: ['https://buttons.github.io/buttons.js'],
-	// You may provide arbitrary config keys to be used as needed by your template.
 	repoUrl: 'https://github.com/Cretezy/noderize',
-	cname: "noderize.js.org"
+	// cname: "noderize.js.org"
 };
 
 module.exports = siteConfig;
